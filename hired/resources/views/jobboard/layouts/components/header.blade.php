6fa90d8f1dcc80274e89e2df69698acf1a942ab8
l37sg0 <petar_1v@abv.bg>
2023-01-30 20:06:43 +0200

    [#3]  - template for jobs and gigs with cards, badges, thimbnail and pricing.

---
+++ hired/resources/views/jobboard/layouts/components/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="mb-auto">
+        <h3 class="float-md-start mb-0">Hired</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Jobs</a>
+            <a class="nav-link fw-bold py-1 px-0" href="#">Gigs</a>
+            <a class="nav-link fw-bold py-1 px-0" href="#">Login</a>
+            <a class="nav-link fw-bold py-1 px-0" href="#">Register</a>
+        </nav>
+    </div>
+</header>
+<div class="px-3">
+    <h1>Jobs</h1>
+    <p class="lead">Find your next freelance project here.</p>
+</div>
