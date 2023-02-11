1c5f5e7af963f5be2ebf02d6171fadd43e513239
l37sg0 <petar_1v@abv.bg>
2023-02-11 12:50:56 +0200

    [#2]  - migrations for portfolio models.

---
+++ hired/database/migrations/2023_02_11_080327_create_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Globals;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\JobBoard\Portfolio\Portfolio as Model;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Model::TABLE_NAME, function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(Model::FIELD_USER_ID)
+                ->constrained(User::TABLE_NAME)->onDelete(Globals::ON_DELETE_CASCADE);
+            $table->tinyInteger(Model::FIELD_PORTFOLIO_TYPE);
+            $table->text(Model::FIELD_ABOUT);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(Model::TABLE_NAME);
+    }
+};
